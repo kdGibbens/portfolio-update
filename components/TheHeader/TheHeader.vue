9b58655446ab600ecbcfc1af2eef2c99ebe01727
Kelly Gibbens <gibbenskd@gmail.com>
2018-05-06 13:32:47 -0500

    Added the menu to the header.

--- components/TheHeader/TheHeader.vue
+++ components/TheHeader/TheHeader.vue
@@ -1,11 +1,12 @@
 <template>
-<div class="container header-container">
-    <header class="header">
+<div class="header-container">
+    <header class="header container">
       <div class="header__logo">
           <nuxt-link to="/" tag="div" class="header__logo-link">
               <a><h2 class="logo">{{ headerLogo }}</h2></a>
           </nuxt-link>
       </div>
+      <TheMenu />
       <div class="header__menu-btns">
           <div class="menu__btn--open">
               <font-awesome-icon :icon="['fas', 'bars']" />
@@ -19,12 +20,16 @@
 </template>
 
 <script>
+    import TheMenu from '@/components/TheMenu/TheMenu';
     export default {
         name: 'the-header',
         data() {
             return {
                 headerLogo: 'kdg',
             }
+        },
+        components: {
+            TheMenu
         }
     }
 </script>
--- pages/About/index.vue
+++ pages/About/index.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    <h1>About Me</h1>
+  <div class="about">
+    <div class="left">
+      <h1 class="heading-1 bold">About Me</h1>
+    </div>
+    <div class="right">
+      <div class="about__text">
+        <p class="paragraph">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac magna vel ex pharetra consequat vitae sed nunc. Quisque congue eros sem, at sollicitudin mauris euismod eu. Cras eget arcu ut lacus faucibus rhoncus. Pellentesque volutpat feugiat elementum. Sed a ligula in lorem rutrum condimentum eget id neque. Pellentesque fermentum vitae elit non lacinia. Integer vitae condimentum tortor, id auctor quam. Nullam hendrerit sed metus ac interdum. Nam eu tincidunt lacus, in scelerisque diam. Aliquam mattis, diam vitae pellentesque pulvinar, mi nulla luctus diam, at cursus leo arcu in quam. Cras et neque enim. Vivamus aliquet eleifend justo vel egestas. Etiam in iaculis ante. Maecenas tincidunt ultricies ex, ac consequat eros tincidunt vel.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac magna vel ex pharetra consequat vitae sed nunc. Quisque congue eros sem, at sollicitudin mauris euismod eu. Cras eget arcu ut lacus faucibus rhoncus. Pellentesque volutpat feugiat elementum. Sed a ligula in lorem rutrum condimentum eget id neque. Pellentesque fermentum vitae elit non lacinia. Integer vitae condimentum tortor, id auctor quam. Nullam hendrerit sed metus ac interdum. Nam eu tincidunt lacus, in scelerisque diam. Aliquam mattis, diam vitae pellentesque pulvinar, mi nulla luctus diam, at cursus leo arcu in quam. Cras et neque enim. Vivamus aliquet eleifend justo vel egestas. Etiam in iaculis ante. Maecenas tincidunt ultricies ex, ac consequat eros tincidunt vel.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
