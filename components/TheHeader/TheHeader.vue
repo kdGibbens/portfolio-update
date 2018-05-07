4e2a0afbff8306cea62d22eb6802edde51204e12
Kelly Gibbens <gibbenskd@gmail.com>
2018-05-06 23:02:17 -0500

    updated styles

--- components/TheHeader/TheHeader.vue
+++ components/TheHeader/TheHeader.vue
@@ -1,20 +1,10 @@
 <template>
 <div class="header-container">
     <header class="header container">
-      <div class="header__logo">
-          <nuxt-link to="/" tag="div" class="header__logo-link">
-              <a><h2 class="logo">{{ headerLogo }}</h2></a>
-          </nuxt-link>
-      </div>
+        <nuxt-link to="/" tag="a" class="header__logo-link">
+          <h2 class="header__logo">{{ headerLogo }}</h2>
+        </nuxt-link>
       <TheMenu />
-      <div class="header__menu-btns">
-          <div class="menu__btn--open">
-              <font-awesome-icon :icon="['fas', 'bars']" />
-          </div>
-          <div class="menu__btn--close">
-              <font-awesome-icon :icon="['fas', 'times']" />
-          </div>
-      </div>
   </header>
 </div>
 </template>
--- components/TheMenu/TheMenu.vue
+++ components/TheMenu/TheMenu.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="menu">
-        <nav class="menu__nav">
-            <ul class="nav__list-items">
+        <nav class="menu__nav nav">
                 <!--Setup as for loop-->
-                <nuxt-link to="/" tag="li" class="list-item"><a>Home</a></nuxt-link>
-                <nuxt-link to="/about" tag="li" class="list-item"><a>About</a></nuxt-link>
-                <nuxt-link to="/portfolio" tag="li" class="list-item"><a>Portfolio</a></nuxt-link>
-            </ul>
+                <nuxt-link to="/" tag="a" class="nav-item">Home</nuxt-link>
+                <nuxt-link to="/about" tag="a" class="nav-item">About</nuxt-link>
+                <nuxt-link to="/portfolio" tag="a" class="nav-item">Portfolio</nuxt-link>
         </nav>
+        <div class="menu__btns">
+          <div class="menu__btn--open menu__btn">
+              <font-awesome-icon :icon="['fas', 'bars']" />
+          </div>
+          <div class="menu__btn--close menu__btn">
+              <font-awesome-icon :icon="['fas', 'times']" />
+          </div>
+      </div>
     </div>
 </template>
 
