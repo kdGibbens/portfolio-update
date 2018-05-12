d5487d0f530f08f9686ed3c0ed5c496a1213c81a
Kelly Gibbens <gibbenskd@gmail.com>
2018-05-12 12:44:09 -0500

    Changed fields to vue data to get ready to update with actual content.

--- pages/About/index.vue
+++ pages/About/index.vue
@@ -6,19 +6,17 @@
           <img src="https://www.syncron.com/wp-content/uploads/2017/03/img-placeholder.png?x31871" alt="">
         </div>
         <div class="card__info">
-          <p>Kelly Gibbens</p>
-          <p>Front-end Web Developer</p>
-          <p><a href="mailto:gibbenskd@gmail.com">gibbenskd@gmail.com</a></p>
+          <p>{{name}}</p>
+          <p>{{title}}</p>
+          <p><a :href="mailTo">{{email}}</a></p>
         </div>
       </div>
     </div>
     <div class="right">
       <div class="about__text">
+        <h3>{{about.title}}</h3>
         <p class="paragraph">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac magna vel ex pharetra consequat vitae sed nunc. Quisque congue eros sem, at sollicitudin mauris euismod eu. Cras eget arcu ut lacus faucibus rhoncus. Pellentesque volutpat feugiat elementum. Sed a ligula in lorem rutrum condimentum eget id neque. Pellentesque fermentum vitae elit non lacinia. Integer vitae condimentum tortor, id auctor quam. Nullam hendrerit sed metus ac interdum. Nam eu tincidunt lacus, in scelerisque diam. Aliquam mattis, diam vitae pellentesque pulvinar, mi nulla luctus diam, at cursus leo arcu in quam. Cras et neque enim. Vivamus aliquet eleifend justo vel egestas. Etiam in iaculis ante. Maecenas tincidunt ultricies ex, ac consequat eros tincidunt vel.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac magna vel ex pharetra consequat vitae sed nunc. Quisque congue eros sem, at sollicitudin mauris euismod eu. Cras eget arcu ut lacus faucibus rhoncus. Pellentesque volutpat feugiat elementum. Sed a ligula in lorem rutrum condimentum eget id neque. Pellentesque fermentum vitae elit non lacinia. Integer vitae condimentum tortor, id auctor quam. Nullam hendrerit sed metus ac interdum. Nam eu tincidunt lacus, in scelerisque diam. Aliquam mattis, diam vitae pellentesque pulvinar, mi nulla luctus diam, at cursus leo arcu in quam. Cras et neque enim. Vivamus aliquet eleifend justo vel egestas. Etiam in iaculis ante. Maecenas tincidunt ultricies ex, ac consequat eros tincidunt vel.
+          {{about.description}}
         </p>
       </div>
     </div>
@@ -26,5 +24,18 @@
 </template>
 
 <script>
-    
+    export default {
+      data() {
+        return {
+          name: 'Kelly Gibbens',
+          title: 'Front-end UI Developer',
+          email: 'gibbenskd@gmail.com',
+          mailTo: 'mailto:gibbenskd@gmail.com',
+          about: {
+            title: "Hi, my name is Kelly and I'm a Front-end Developer",
+            description: 'About me...'
+          }
+        }
+      }
+    }
 </script>
